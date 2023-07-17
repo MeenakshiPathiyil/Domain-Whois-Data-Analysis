10c82dd3925ba5c96c65d3036c2dfe77581cec9f
Meenu2019 <54184349+Meenu2019@users.noreply.github.com>
2023-07-17 15:25:46 +0530

    Add comments

--- DNS-Analyzer V1.0.py
+++ DNS-Analyzer V1.0.py
@@ -19,23 +19,29 @@ contents_whois = domain_stripped.apply(keys_whois)
 
 df = pd.DataFrame(columns=contents_whois[0], dtype=object)
 
-
 # print (df)
 
-
+# Define a function to retrieve values from WHOIS for a given domain
 def values_whois(domain):
-    stripped_domain = domain.strip()
-    whois_domain = whois.whois(stripped_domain)
-    return list(whois_domain.values())
+    stripped_domain = domain.strip()  # Remove leading and trailing whitespace from the domain
+    whois_domain = whois.whois(stripped_domain)  # Perform WHOIS lookup for the domain
+    return list(whois_domain.values())  # Return a list of WHOIS values
 
-
+# Apply the 'values_whois' function to each domain in the 'domain_stripped' list
 content_whois = domain_stripped.apply(values_whois)
 
+# Get the length of the 'domain_stripped' list
 length = len(domain_stripped)
-i = 0
-while i < length:
-    df.loc[i] = content_whois[i]
-    i += 1
 
+i = 0
+
+# Iterate through each index 'i' until the end of the list
+while i < length:
+    df.loc[i] = content_whois[i]  # Assign the WHOIS values to the corresponding row in the DataFrame 'df'
+    i += 1  # Increment the counter variable
+
+# Filter the DataFrame 'df' to create a new DataFrame 'country_df' containing only rows where the 'country' column is 'US'
 country_df = df[df["country"] == 'US']
+
+# Filter the DataFrame 'df' to create a new DataFrame 'registrar_df' containing only rows where the 'registrar' column is 'CSC CORPORATE DOMAINS, INC.'
 registrar_df = df[df["registrar"] == 'CSC CORPORATE DOMAINS, INC.']
--- DNS-Analyzer V2.0.py
+++ DNS-Analyzer V2.0.py
@@ -1,15 +1,25 @@
 import pandas as pd
 
+# Read the CSV file 'whois-db-download-info-sample.csv' into a DataFrame called 'df'
 df = pd.read_csv('whois-db-download-info-sample.csv')
 
+# Filter the DataFrame 'df' to create a new DataFrame 'df_registrar' containing only rows where the 'registrarName' column is 'GoDaddy.com, LLC'
 df_registrar = df[df["registrarName"] == 'GoDaddy.com, LLC']
 
+# Filter the DataFrame 'df' to create a new DataFrame 'df_createdate' containing only rows where the 'createdDate' column is greater than '2015-05-10'
 df_createdate = df[df["createdDate"] > '2015-05-10']
 
+# Get the unique values in the 'registrarName' column of the DataFrame 'df' and store them in the 'unique_registrar' variable
 unique_registrar = df.registrarName.unique()
 
+# Find duplicate values in the 'registrarName' column of the DataFrame 'df'
 duplicates = df['registrarName'].duplicated(keep=False)
+
+# Create a new DataFrame 'df_unique' containing only the rows from 'df' that are not duplicated in the 'registrarName' column
 df_unique = df[~duplicates]
 
+# Get the number of rows in the DataFrame 'df' and store it in the 'num_rows' variable
 num_rows = len(df)
+
+# Get the number of columns in the DataFrame 'df' and store it in the 'num_cols' variable
 num_cols = len(df.columns)
